2baa617a52455bed282fc1ad443d93514f7dd85a
unknown <samkaris75@gmail.com>
2016-10-29 17:26:06 -0700

    to correct the test for raising ZeroDivisionError when num1 = 0

--- unittest_calc.py
+++ unittest_calc.py
@@ -1,20 +1,23 @@
 import unittest
 
-from  calc import try_1
+from calc import try_1
 
-class TestTry1(unitest.TestCase):
+class TestTry1(unittest.TestCase):
 	def test_addition(self):
-		self.assertEqual(calc.try_1(5, 3, "add"), 8)
+		self.assertEqual(try_1(5, 3, "add"), 8)
 
 	def test_substraction(self):
-		self.assertEqual(calc.try_1(5, 3, "subtract"), 2)
+		self.assertEqual(try_1(5, 3, "subtract"), 2)
 
 	def test_multiplication(self):
-		self.assertEqual(calc.try_1(5, 3, "multiply"), 15)
+		self.assertEqual(try_1(5, 3, "multiply"), 15)
 
 	def test_division(self):
-		self.assertEqual(calc.try_1(10, 3, "divide"), 3.33)
+		self.assertEqual(try_1(10, 3, "divide"), 3.33)
 
 	def test_division_by_zero(self):
-		with.self.assertRaises(ZeroDivisionError):
-			calc.try_1(5, 3, "divide")
+		with self.assertRaises(ZeroDivisionError):
+			try_1(5, 0, "divide")
+
+if __name__ == '__main__':
+	unittest.main()
